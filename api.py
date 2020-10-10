e5267d0313ed917e7f6aa3ee973f23a8d75bffa9
Areez Visram <visraa7@mcmaster.ca>
2020-10-10 18:02:15 -0400

    new applications schema

--- api.py
+++ api.py
@@ -6,6 +6,7 @@ from passlib.apps import custom_app_context as pwd_context
 from itsdangerous import (TimedJSONWebSignatureSerializer
                           as Serializer, BadSignature, SignatureExpired)
 from flask_cors import CORS, cross_origin
+from flask_migrate import Migrate
 
 # initialization
 app = Flask(__name__)
@@ -17,6 +18,7 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 # extensions
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 auth = HTTPBasicAuth()
 
 
@@ -47,6 +49,29 @@ class User(db.Model):
             return None    # invalid token
         user = User.query.get(data['id'])
         return user
+
+class JobApplication(db.Model):
+    __tablename__ = 'job_applications'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    position = db.Column(db.String(255), nullable=False)
+    company = db.Column(db.String(255), nullable=False)
+    link_to_application = db.Column(db.String(255), nullable=True)
+    application_deadline = db.Column(db.String(255), nullable=True)
+    application_status = db.Column(db.String(255), nullable=False)
+    portal_password = db.Column(db.String(255), nullable=True)
+    documents_required = db.Column(db.String(255), nullable=False)
+
+
+    def __init__(self, user_id, position, company, link_to_application, application_deadline, application_status, portal_password, documents_required):
+        self.user_id = user_id
+        self.position = position
+        self.company = company
+        self.link_to_application = link_to_application
+        self.application_deadline = application_deadline
+        self.application_status = application_status
+        self.portal_password = portal_password
+        self.documents_required = documents_required
 
 
 @auth.verify_password
@@ -97,11 +122,99 @@ def get_auth_token():
 @app.route('/api/resource')
 @auth.login_required
 def get_resource():
-
     # change the last argument to get specific attribute
-    return jsonify({'data': g.user.username})
+    return jsonify({'name': g.user.username, 'id': g.user.id})
+
+@app.route('/api/getApplications', methods=["POST"])
+def get_applications():
+    try:
+        req_json = request.get_json()
+        curr_user = req_json['id']
+    except:
+        response = {'status': 0, 'error': 'No jobs exist for this user.'}
+        return jsonify(response)
+    
+    user_applications = JobApplication.query.filter_by(user_id=curr_user)
+    user_applications_dict ={'status': 1, 'applications': []}
+
+    for application in user_applications:
+        user_applications_dict['applications'].append(makeJson(dict(application.__dict__)))
+    
+    return jsonify(user_applications_dict)
+    
+
+@app.route('/api/addApplication', methods=["POST"])
+def add_application():
+    try:
+        req_json = request.get_json()
+
+        curr_application_user_id = req_json['id'] if req_json['id'] else None
+        curr_application_position = req_json['position'] if req_json['position'] else None
+        curr_application_company = req_json['company'] if req_json['company'] else None
+        curr_link_to_application = req_json['link_to_application'] if req_json['link_to_application'] else None
+        curr_application_deadline = req_json['application_deadline'] if req_json['application_deadline'] else None
+        curr_application_status = req_json['application_status'] if req_json['application_status'] else None
+        curr_portal_password = req_json['portal_password'] if req_json['portal_password'] else None
+        curr_documents_required = req_json['documents_required'] if req_json['documents_required'] else None
+
+
+    except:
+        response = {'status': 0, 'error': 'KeyError. Missing parameters'}
+        return jsonify(response)
+    
+    try:
+        application = JobApplication(curr_application_user_id, curr_application_position, curr_application_company, curr_link_to_application, curr_application_deadline, 
+            curr_application_status, curr_portal_password, curr_documents_required)
+        db.session.add(application)
+        db.session.commit()
+        response = {'status': 1}
+    except:
+        response = {'status': 0, 'error': 'Database error. Check for empty fields'}
+        return jsonify(response)
+    return jsonify(response)
+
+# Global
+def makeJson(result):
+    del result['_sa_instance_state']
+    return result
+
+# VIEWS
+@app.route('/viewUsers')
+def users_view():
+    users = User.query.all()
+    user_dict = {'users': []}
+
+    for user in users:
+        user_dict['users'].append(makeJson(dict(user.__dict__)))
+
+    return jsonify(user_dict)
+
+@app.route('/viewApplications')
+def applications_view():
+    applications = JobApplication.query.all()
+    application_dict = {'applications': []}
+
+    for application in applications:
+        application_dict['applications'].append(makeJson(dict(application.__dict__)))
+    
+    return jsonify(application_dict)
+
+@app.route('/api/deleteApplication')
+def application_delete():
+    req_json = request.get_json()
+    application_id = req_json['id']
+
+    application = JobApplication.query.filter_by(id=application_id).first()
+
+    db.session.delete(application)
+    db.session.commit()
+
+    response = {'status': 1, 'message': "Record deleted"}
+
+    return jsonify(response)
 
 
 @app.before_first_request
 def create_tables():
     db.create_all()
+
